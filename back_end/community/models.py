27ec9cd7fdcb358f79b13ac1a5041bddb555a421
Lee-hanbin <hanbin0501@gmail.com>
2022-11-16 17:54:05 +0900

    create model

--- back_end/community/models.py
+++ back_end/community/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.conf import settings
+from movies.models import Movie
 
 # Create your models here.
 class Article(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- back_end/movies/models.py
+++ back_end/movies/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+class Actor(models.Model):
+    name = models.CharField(max_length=50)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    release_date = models.DateField()
+    popularity = models.FloatField()
+    vote_average = models.FloatField()
+    poster_path = models.CharField(max_length=200, default=None)
+    genres = models.ManyToManyField(Genre)
+    actores = models.ManyToManyField(Actor)
+
+    def __str__(self):
+        return self.title
