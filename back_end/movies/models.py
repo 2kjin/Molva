6ac4598af94c26191a11de9b12da190039125795
Lee-hanbin <hanbin0501@gmail.com>
2022-11-17 09:21:25 +0900

    upload back

--- back_end/movies/models.py
+++ back_end/movies/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Genre(models.Model):
-    name = models.CharField(max_length=50)
+# class Genre(models.Model):
+#     name = models.CharField(max_length=50)
 
-class Actor(models.Model):
-    name = models.CharField(max_length=50)
+# class Actor(models.Model):
+#     name = models.CharField(max_length=50)
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
@@ -15,8 +15,8 @@ class Movie(models.Model):
     popularity = models.FloatField()
     vote_average = models.FloatField()
     poster_path = models.CharField(max_length=200, default=None)
-    genres = models.ManyToManyField(Genre)
-    actores = models.ManyToManyField(Actor)
+    # genres = models.ManyToManyField(Genre)
+    # actores = models.ManyToManyField(Actor)
 
     def __str__(self):
         return self.title
